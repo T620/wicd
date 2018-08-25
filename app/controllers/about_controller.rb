123448ca6f42f81ea35751a690dce0ed0c840bcc
Josh Tait <Josh.tait3@gmail.com>
2018-08-25 18:34:12 +0100

    modify controllers

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,4 +2,9 @@ class AboutController < ApplicationController
   def index
     render 'index'
   end
+
+  #don't want another controller just to render a contact page
+  def contact
+    render 'contact/form'
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,11 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Projects.all
     render 'index'
   end
   
   def show
+    @project = Project.find(params[:id])
     render 'show'
   end
 end
